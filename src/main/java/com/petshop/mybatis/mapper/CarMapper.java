f78b5c0b8dd18b3dfc94efb2a3d7915cd42c62ef
AKuteynitsin <AKuteynitsin@IBS.ru>
2019-03-21 18:45:40 +0300

    queries with xml

--- src/main/java/com/petshop/mybatis/mapper/CarMapper.java
+++ src/main/java/com/petshop/mybatis/mapper/CarMapper.java
@@ -7,16 +7,8 @@ import java.util.List;
 
 @Mapper
 public interface CarMapper {
-    @Select("SELECT * FROM CAR")
     List<Car> findAll();
-
-    @Insert("INSERT INTO CAR(model, code, color) VALUES(#{model}, #{code}, #{color})")
-    @Options(useGeneratedKeys = true, keyColumn="CAR.id")
     void create(Car car);
-
-    @Update("UPDATE CAR SET model=#{model}, code=#{code}, color=#{color} WHERE id=#{id}")
     void update(Car car);
-
-    @Delete("DELETE FROM CAR WHERE id=#{id}")
     void remove(Car car);
 }
